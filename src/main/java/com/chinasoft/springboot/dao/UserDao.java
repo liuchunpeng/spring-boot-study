d6add1f3a6ced87ee4f95654be629e0a068b3de8
admin <1507550938@qq.com>
2020-01-03 17:56:48 +0800

    asd

--- src/main/java/com/chinasoft/springboot/dao/UserDao.java
+++ src/main/java/com/chinasoft/springboot/dao/UserDao.java
@@ -2,13 +2,7 @@ package com.chinasoft.springboot.dao;
 
 import com.chinasoft.springboot.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserDao {
-    /**
-     * @param userName
-     * @return
-     */
+public interface UserDao extends JpaRepository<User, Integer> {
     User findByUserName(String userName);
 }
