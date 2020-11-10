c0c9a350a006181fe759445cb5a2ad6cae200e47
admin <1507550938@qq.com>
2020-11-10 16:09:36 +0800

    db

--- src/main/java/com/chinasoft/springboot/entities/User.java
+++ src/main/java/com/chinasoft/springboot/entities/User.java
@@ -12,4 +12,27 @@ public class User implements Serializable {
 
     private String passWord;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
 }
